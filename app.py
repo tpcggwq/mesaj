a181c662212857b333c34cd50c36c16ad3f32076
tpcggwq <kayuaben@gmail.com>
2025-05-12 03:54:47 +0300

    Arka plan resmi eklendi

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 import os
 
+# Flask uygulamasını oluştur (EN ÜSTTE OLMALI)
 app = Flask(__name__)
-from flask import send_from_directory
 
-# app.py'de bu ayarı ekleyin
+# Konfigürasyon ayarları
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0  # Önbellek sorununu çözer
+
+# Route tanımları
+@app.route('/protected_images/<filename>')
+def protected_images(filename):
+    return send_from_directory('static/images', filename)
 
 @app.route('/test-images')
 def test_images():
     image_list = os.listdir('static/images')
     return f"Sunucudaki resimler: {image_list}"
+
 @app.route('/')
 def home():
     try:
