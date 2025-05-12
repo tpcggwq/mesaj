8946d86c5082ccfdd8067185c20b48a71e27c547
tpcggwq <kayuaben@gmail.com>
2025-05-12 03:29:19 +0300

    Fotoğraflar ve açıklamalar eklendi

--- app.py
+++ app.py
@@ -2,7 +2,15 @@ from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
+from flask import send_from_directory
 
+# app.py'de bu ayarı ekleyin
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0  # Önbellek sorununu çözer
+
+@app.route('/test-images')
+def test_images():
+    image_list = os.listdir('static/images')
+    return f"Sunucudaki resimler: {image_list}"
 @app.route('/')
 def home():
     try:
