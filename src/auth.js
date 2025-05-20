0aaee555a1c972107cbfdb858c249f0507738b61
tpcggwq <kayuaben@gmail.com>
2025-05-20 23:02:53 +0300

    Update auth.js

--- src/auth.js
+++ src/auth.js
@@ -1,1 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDCnBMVNe3A9LPGbO_ZhExn5TKqWLz7WuU",
+  authDomain: "mesaj-f1916.firebaseapp.com",
+  projectId: "mesaj-f1916",
+  storageBucket: "mesaj-f1916.appspot.com",
+  messagingSenderId: "536452620539",
+  appId: "1:536452620539:web:82a98f87008cf0eef0505e"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
