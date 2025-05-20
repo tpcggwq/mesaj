7911b7a18b2e08236f219c69c7d6daa4659d6935
tpcggwq <kayuaben@gmail.com>
2025-05-20 23:12:06 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Static dosyaları göster (CSS, JS, HTML)
+app.use(express.static('public'));
+
+// Tüm istekleri index.html'e yönlendir
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => console.log(`Çalışıyor: http://localhost:${PORT}`));
